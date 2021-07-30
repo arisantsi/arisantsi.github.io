d08d44166ccc49c8f57dc539de558de7b01c6716
Anis Fikriyyah <38931702+anisfikriyyah@users.noreply.github.com>
2021-07-30 15:31:08 +0700

    Add files via upload

--- constants.js
+++ constants.js
@@ -1,9 +1,12 @@
 // Options the user could type in
 var today = new Date();
 var time = today.getHours() + " lewat " + today.getMinutes() + " menit";
+var days = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum at', 'Sabtu'];
+var dayName = days[today.getDay()];
 
 const prompts = [
   ["password", "lupa"],
+  ["sekarang hari apa?", "hari apa?", "hari"],
   ["quote", "motivasi", "alquran", "quran", "ayat", "ngaji"],
   ["hi", "halo", "selamat siang"],
   ["Anis", "anis", "halo anis"],
@@ -15,6 +18,7 @@ const prompts = [
 
 const replies = [
   ["Ulang tahun anis", "ulang tahun tisha", "19 12 94"],
+  [`Hari ini hari ${dayName}`]
   ["Apa kamu sedang cemas?"],
   ["iya, ada yang dapat kami bantu?"],
   ["Ya ada apa?", "Itu namaku, cantik bukan?", "Kamu jangan terlalu berharap, suamiku asruldev"],
